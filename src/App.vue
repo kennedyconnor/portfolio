08ecca904981cc3a4fbe1bea7f042368c005818d
Connor Kennedy <kennedyconnor02@gmail.com>
2019-09-26 23:30:27 -0600

    basic views

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div class="container-fluid" id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/resume">Resume</router-link>
     </div>
     <router-view />
   </div>
---
+++ src/views/Resume.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="resume">
+    <h1>My Resume</h1>
+
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+
+
+  export default {
+    name: 'resume',
+    components: {
+
+    }
+  }
+</script>
