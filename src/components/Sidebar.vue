2fd0916d73e7e99d50304f26abb6a0ae395122c3
Connor Kennedy <kennedyconnor02@gmail.com>
2019-10-01 23:55:20 -0600

    sb2

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="sidebar">
+    <div>
+      <h1>Connor Kennedy</h1>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+
+
+  export default {
+    name: 'sidebar',
+    components: {
+
+    }
+  }
+</script>
+<style>
+
+</style>
