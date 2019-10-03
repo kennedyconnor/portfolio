675370f530a19c8aed828372a4e77139efd8062e
Connor Kennedy <kennedyconnor02@gmail.com>
2019-10-02 23:59:39 -0600

    asd

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="col-3" id="aboutMe">
+    <div>
+      <h1>Connor Kennedy</h1>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+
+
+  export default {
+    name: 'aboutMe',
+    components: {
+
+    }
+  }
+</script>
+<style>
+
+</style>
