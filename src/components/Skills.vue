105b1922ab1043e2ff6cbd0d9991a4c651c827a9
Connor Kennedy <kennedyconnor02@gmail.com>
2019-10-04 00:00:09 -0600

    cardskill

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,10 +2,54 @@
   <div class="col" id="skills">
     <h3>skills</h3>
     <div class="row">
-      <div class="col-3">Skill</div>
-      <div class="col-3">Skill</div>
-      <div class="col-3">Skill</div>
-      <div class="col-3">Skill</div>
+      <div class="col-3">
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">skill</h5>
+            <p class="card-text">Some quick example text to build on the skill and make up the bulk of the card's
+              content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">skill</h5>
+            <p class="card-text">Some quick example text to build on the skill and make up the bulk of the card's
+              content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">skill</h5>
+            <p class="card-text">Some quick example text to build on the skill and make up the bulk of the card's
+              content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">skill</h5>
+            <p class="card-text">Some quick example text to build on the skill and make up the bulk of the card's
+              content.
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="row">
       <div class="col-3">Skill</div>
