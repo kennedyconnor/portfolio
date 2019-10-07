8a89b6fe7d02202c87cbce7adbdb7e31486a99da
Connor Kennedy <kennedyconnor02@gmail.com>
2019-10-06 23:20:16 -0600

    working on it!

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -4,8 +4,9 @@
     <div class="row">
       <div class="col-3">
         <div class="card">
-          <img src="https://place-hold.it/100x100" class="card-img-top" alt="">
+
           <div class="card-body">
+
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
               content.
             </p>
