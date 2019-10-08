a648f7fba64571352394b84ea2022d19d3a8de85
Connor Kennedy <kennedyconnor02@gmail.com>
2019-10-07 23:57:48 -0600

    no more ugly fonts bruh

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <style>
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: Georgia, serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
