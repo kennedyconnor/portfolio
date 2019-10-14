decdab2a4d5de512e734f49e608f2b028cce3dd6
Connor Kennedy <kennedyconnor02@gmail.com>
2019-10-13 23:59:47 -0600

    p4olio

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,6 +2,52 @@
   <div class="col">
     <h3>Projects
     </h3>
+    <div class="row">
+      <div class="col-4 project-col">
+        <div class="card project-link" style="width: 18rem;">
+          <img class="card-img-top" src="" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Project name</h5>
+            <p class="card-text">Description of project
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-4 project-col">
+        <div class="card project-link" style="width: 18rem;">
+          <img class="card-img-top" src="" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Project Name</h5>
+            <p class="card-text">Description of project
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-4 project-col">
+        <div class="card project-link" style="width: 18rem;">
+          <img class="card-img-top" src="" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Project Name</h5>
+            <p class="card-text">Description of project
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-4 project-col">
+        <div class="card project-link" style="width: 18rem;">
+          <img class="card-img-top" src="" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Project Name</h5>
+            <p class="card-text">Description of project
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
