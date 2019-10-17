e98dd04be98a675c94e92c0c808eef3d06217b87
Connor Kennedy <kennedyconnor02@gmail.com>
2019-10-16 23:52:20 -0600

    working on hosting resume

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -2,13 +2,7 @@
   <div class="row" id="resume">
     <Sidebar />
     <div class="col">
-      <h3>Connor Kennedy</h3>
-      <h5>About</h5>
-      <p>Lorem Ipsum</p>
-
-      <h5>Skills</h5>
-      <h5>Work Experience</h5>
-      <h5>Education</h5>
+      <embed src="../assets/ConnorKennedyResume.pdf" type="">
     </div>
 
   </div>
